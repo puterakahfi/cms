74f047838a11ca88211af9d620595e60835ea5ea
puterakahfi <puterakahfi@gmail.com>
2019-09-03 16:06:06 +0700

    Updates

---
+++ assets/js/page--src--pages--index-vue.97a1ca07.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{230:function(n,t,e){},231:function(n,t,e){"use strict";var u=e(230);e.n(u).a},233:function(n,t,e){"use strict";e.r(t);var u={metaInfo:{title:"Gridsome Course Landingpage!"}},a=(e(231),e(1)),i=Object(a.a)(u,function(){var n=this.$createElement;return(this._self._c||n)("Layout")},[],!1,null,null,null);t.default=i.exports}}]);
